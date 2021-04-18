7e7a0041bbca104a84c488ace09b21666f8277c8
Younus Ali Akash <younusaliakash8@gmail.com>
2021-04-18 22:46:05 +0600

    done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT ||5000;
 
 app.use(cors());
 app.use(bodyParser.json());
